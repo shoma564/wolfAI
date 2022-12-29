1ab46c22dfd51ed21e18bea6dc7895b85b9bfb9f
shoma564 <shomaigu@gmail.com>
2022-12-29 07:03:18 +0000

    dbのDockerfile化

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,12 @@ docker-compose down --rmi all --volumes --remove-orphans
 
 rm -r mysql/*
 
+
+#docker build ./flask -t shomaigu/pro-flask-slack:latest
+#docker push shomaigu/pro-flask-slack:latest
+
+docker build ./db -t shomaigu/pro-mysql:latest
+docker push shomaigu/pro-mysql:latest
+
+
 docker-compose up
